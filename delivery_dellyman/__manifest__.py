0967450b4c6eeaf1dc1dc7fb7852f65819ae94ce
Babatope Ajepe <ajepebabatope@gmail.com>
2022-04-20 12:39:17 +0100

    [UPD] update delivery_dellyman

--- delivery_dellyman/__manifest__.py
+++ delivery_dellyman/__manifest__.py
@@ -1,35 +1,22 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "delivery_dellyman",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
+    "name": "Dellyman Delivery",
+    "summary": """Dellyman Delivery""",
+    "description": """
+        Dellyman Logistics Integration
     """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base', 'sale_stock'],
-
+    "author": "Matt O'Bell",
+    "website": "http://www.yourcompany.com",
+    "category": "Uncategorized",
+    "version": "11.0.1.0.0",
+    "depends": ["base", "sale_stock", "delivery", "base_geolocalize"],
     # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+    "data": [
+        "security/ir.model.access.csv",
+        "data/delivery_data.xml",
+        "views/delivery_carrier.xml",
+        "views/views.xml",
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    "external_dependencies": {
+        "python": ["geopy"],
+    },
 }
--- delivery_dellyman/models/delivery_carrier.py
+++ delivery_dellyman/models/delivery_carrier.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
 import json
 import requests
 from werkzeug.urls import url_join
@@ -7,10 +5,14 @@ from werkzeug.urls import url_join
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError
 from odoo.tools.float_utils import float_round
+from odoo.tools.misc import flatten
+import requests
 
 import urllib
 import datetime
 from statistics import mean
+
+from geopy import distance
 
 TIMEOUT = 20
 REQUEST_HEADER = {"Content-type": "application/json"}
@@ -19,142 +21,118 @@ REQUEST_HEADER = {"Content-type": "application/json"}
 class DeliverCarrier(models.Model):
     _inherit = "delivery.carrier"
 
+    dellyman_api_key = fields.Char(string="API Key")
     delivery_type = fields.Selection(selection_add=[("dellyman", "Dellyman")])
+    dellyman_base_price = fields.Float(string="Base Price", required=False, default=500)
+    dellyman_rate_per_km = fields.Float(
+        default=20, required=False, string="Rate Per Km"
+    )
+    dellyman_base_distance = fields.Float(
+        default=10, required=False, string="Base Distance"
+    )
+    dellyman_algorithm_ids = fields.One2many(
+        comodel_name="dellyman.algorithm",
+        inverse_name="delivery_carrier_id",
+        ondelete="cascade",
+    )
 
-    dellyman_base_price = fields.Float(string="Price", default=100, help="Base price")
+    def get_distance_between_store_and_delivery(self, order):
+        partner_shipping_id = order.partner_shipping_id
 
-    dellyman_baseurl = fields.Char(string="Base URL")
-    dellyman_apiid = fields.Char(string=" API ID")
-    dellyman_apisecret = fields.Char(string="API Secret")
-    dellyman_token = fields.Char(string="Token")
-    dellyman_customer_auth = fields.Char(string="Customer Auth")
-    dellyman_customer_id = fields.Char(string="Customer ID")
-    dellyman_name = fields.Char(string="Username")
-    dellyman_email = fields.Char(string="Email")
-    dellyman_password = fields.Char(string="Password")
-    dellyman_phonenumber = fields.Char(string="Phone")
-    dellyman_company_id = fields.Char(string="Delivery CompanyID", default=643)
+        partner_latitude = partner_shipping_id.partner_latitude
+        partner_longitude = partner_shipping_id.partner_longitude
 
-    def action_get_dellyman_token(self):
-        """ 
-        @API call response 
-            {
-                "authentication_token": "xda8qt147uf72s1z1mp06xafcln9o35tae9ovkiw",
-                "ResponseCode": 100,
-                "ResponseMessage": "Authentication Success",
+        company_latitude = self.company_id.partner_id.partner_latitude
+        company_longitude = self.company_id.partner_id.partner_longitude
+        if not partner_latitude or not partner_longitude:
+            partner_shipping_id.geo_localize()
+            partner_latitude = partner_shipping_id.partner_latitude
+            partner_longitude = partner_shipping_id.partner_longitude
+
+        if not company_latitude or not company_longitude:
+            self.company_id.partner_id.geo_localize()
+            company_latitude = self.company_id.partner_id.partner_latitude
+            company_longitude = self.company_id.partner_id.partner_longitude
+
+        store = (company_latitude, company_longitude)
+        partner = (partner_latitude, partner_longitude)
+        return distance.distance(partner, store).km
+
+    def dellyman_rate_shipment(self, order):
+        cr = self.env.cr
+        carrier = self._match_address(order.partner_shipping_id)
+        if not carrier:
+            return {
+                "success": False,
+                "price": 0.0,
+                "error_message": _(
+                    "Error: this delivery method is not available for this address."
+                ),
+                "warning_message": False,
             }
-        """
+        amount_total = order.amount_total
+        sql = """SELECT percentage * 0.01  FROM dellyman_algorithm WHERE %s  BETWEEN SYMMETRIC from_amount AND to_amount LIMIT 1"""
 
-        data = {"APISecret": self.dellyman_apisecret, "APIID": self.dellyman_apiid}
-        endpoint = urllib.parse.urljoin(self.dellyman_baseurl, "api/v1.0/Authentication")
-
-        try:
-            req = requests.post(endpoint, data=json.dumps(data), headers=REQUEST_HEADER, timeout=TIMEOUT, verify=False,)
-            req.raise_for_status()
-        except requests.HTTPError:
-
-            raise UserError(_("Please make sure that the APIID and API secret field are set"))
-
-        res = req.json()
-        if res.get("ResponseCode") != 100:
-            raise UserError(res)
-        self.dellyman_token = res.get("authentication_token")
-        return res
-
-    def action_get_dellyman_customer_auth(self, response):
-        # after getting the authentication token, we need to validate/login with the token
-        endpoint = urllib.parse.urljoin(self.dellyman_baseurl, "/api/v1.0/CustomerValidation")
-
-        REQUEST_HEADER.update(
-            {
-                "Authorization": "Bearer %s" % self.dellyman_token,
-                "APIID": self.dellyman_apiid,
-                "APISecret": self.dellyman_apisecret,
-            }
+        cr.execute(sql, (amount_total,))
+        percentage = flatten(cr.fetchall())
+        price = (
+            percentage[0] * amount_total
+            if percentage[0] * amount_total > self.dellyman_base_price
+            else self.dellyman_base_price
         )
 
-        payload = {
-            "Email": self.dellyman_email,
-            "Password": self.dellyman_password,
-        }
-        req = requests.post(endpoint, data=json.dumps(payload), headers=REQUEST_HEADER, timeout=TIMEOUT)
-        res = req.json()
-        self.dellyman_customer_auth = res.get("CustomerAuth")
-        self.dellyman_customer_id = res.get("CustomerID")
-        REQUEST_HEADER["CustomerAuth"] = res.get("CustomerAuth")
-        REQUEST_HEADER["CustomerID"] = res.get("CustomerAuth")
-        return res
-
-    def dellyman_authenticate(self):
-        """Dellyman token and customer auth expires after 24 hours.
-
-        """
-        res = self.action_get_dellyman_token()
-        return self.action_get_dellyman_customer_auth(res)
-
-    def dellyman_calculate_amount_values(self, order):
-        partner = order.partner_id
-        company = order.company_id.partner_id
-        partner_address = "%s %s %s %s" % (partner.street, partner.state_id.name, partner.city, partner.country_id.name)
-        company_address = "%s %s %s %s" % (company.street, company.state_id.name, company.city, company.country_id.name)
+        distance = self.get_distance_between_store_and_delivery(order)
+        if distance > self.dellyman_base_distance:
+            distance = distance - self.dellyman_base_price
+            price = price + (distance * self.dellyman_rate_per_km)
 
         return {
-            "CustomerID": int(self.dellyman_customer_id),
-            "VehicleID": 1,
-            "IsCoD": 1,
-            "PickupRequestedTime": "06 AM to 09 PM",
-            "PickupRequestedDate": datetime.date.today().strftime("%d/%m/%Y"),
-            "PickupAddress": partner_address,
-            "DeliveryAddress": [company_address],
+            "success": True,
+            "price": price,
+            "error_message": False,
+            "warning_message": False,
         }
 
-    def dellyman_rate_shipment(self, sale_order):
-        endpoint = urllib.parse.urljoin(self.dellyman_baseurl, "/api/v1.0/CustomerCalculateAmount")
-        data = self.dellyman_calculate_amount_values(sale_order)
-        req = requests.post(endpoint, data=json.dumps(data), headers=REQUEST_HEADER, timeout=TIMEOUT)
-        res = req.json()
-        distance = mean([company.get("Distance") for company in res.get("Companies", [])])
-        price = distance * self.dellyman_base_price
-        return {"success": True, "price": price, "error_message": False, "warning_message": False}
-
     def dellyman_send_shipping(self, pickings):
-        sale_id = pickings.mapped("sale_id")
-        partner = sale_id.partner_id
-        partner_address = "%s %s %s %s" % (partner.street, partner.state_id.name, partner.city, partner.country_id.name)
+        url = "https://dev.dellyman.com/api/v3.0/BookOrder"
+        headers = {
+            "Authorization": "Bearer %s" % (self.dellyman_api_key),
+        }
         payload = {
-            "CustomerID": int(self.dellyman_customer_id),
-            "CompanyID": self.dellyman_company_id,
-            "CustomerAuth": self.dellyman_customer_auth,
-            "VehicleID": 1,
-            "IsCoD": 1,
-            "PickUpContactName": partner.name,
-            "PickUpContactNumber": partner.phone or partner.mobile,
-            "PickUpGooglePlaceAddress": partner_address,
+            "CustomerID": 2,
+            "CustomerAuth": "dfSVhQ8jQh0trncHkdELvwHgskI1Rj0w",
+            "CompanyID": 762,
+            "PaymentMode": "pickup",
+            "Vehicle": "Bike",
+            "PickUpContactName": "Administrator",
+            "PickUpContactNumber": "07068937300",
+            "PickUpGooglePlaceAddress": "3 Allen Avenue Lagos Lagos",
             "PickUpLandmark": " ",
-            "PickUpRequestedDate": datetime.date.today().strftime("%d/%m/%Y"),
-            "PickUpRequestedTime": "06 AM to 09 PM",
-            "DeliveryRequestedTime": "06 AM to 09 PM",
+            "IsInstantDelivery": 1,
+            "PickUpRequestedDate": "",
+            "PickUpRequestedTime": "",
+            "DeliveryRequestedTime": "",
             "Packages": [
                 {
-                    "PackageDescription": "Laptop",
+                    "PackageDescription": "Allen/OUT/00024",
                     "DeliveryContactName": "Babatope Ajepe",
-                    "DeliveryContactNumber": "07068937300",
-                    "DeliveryGooglePlaceAddress": partner_address,
+                    "DeliveryContactNumber": "07055667789",
+                    "DeliveryGooglePlaceAddress": "73 Allen Avenue Ikeja Lagos",
                     "DeliveryLandmark": "",
                 }
                 for picking in pickings
             ],
         }
-        print("***************dellyman_send_shipping*************************", sale_id)
-        raise exceptions
-        return {"exact_price": 5000, "tracking_number": 1111}
+        res = requests.post(url, json=payload, headers=headers)
+        print(res.text, "!!!!!!!!!!!!!!!!!!!!!!!!1")
 
-    def dellyman_get_tracking_link(self):
-        print("******************************************dellyman_get_tracking_link")
-        return False
 
-    # def dellyman_cancel_shipment(self):
-    #     pass
+class DellymanAlgorithm(models.Model):
+    _name = "dellyman.algorithm"
+    _desscription = "Dellyman Algorithm"
 
-    # def dellyman_get_default_custom_package_code(self):
-    #     pass
+    name = fields.Char(string="Name", required=True)
+    to_amount = fields.Integer(string="Price To", required=True)
+    percentage = fields.Float(string="Percentage", required=True)
+    from_amount = fields.Integer(string="Price From", required=True)
+    delivery_carrier_id = fields.Many2one(comodel_name="delivery.carrier")
